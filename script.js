6b86bc68b75495bbd64fe48e58705239fc58b6d9
ksahh <haseebshaikh25ks@gmail.com>
2023-08-28 09:43:45 +0530

    Final commit 1

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+////////////////////////////////////////////////////////////////////////////
+
 let screen = document.querySelector("#screen");
 
 function makeBubble(){
 
     let bubble = '';
-
+    
     for (let i = 1; i <= 96; i++) {
-        bubble += `<div class="bubble">${i}</div>`;
+        let randomNumber = Math.floor((Math.random() * 10) + 1)
+        bubble += `<div class="bubble">${randomNumber}</div>`;
 
     }
-    screen.innerHTML += bubble;
+    screen.innerHTML = bubble;
 }
 
 makeBubble();
 
+////////////////////////////////////////////////////////////////////////////
 
 
 let remainingTime = 61; 
@@ -28,6 +32,7 @@ function updateTimerDisplay() {
         
         if (remainingTime <= 0) {
             clearInterval(interval);
+            screenBottom.innerHTML = `<h1> Game Over your score is ${score} </h1>`
             console.log("Timer has reached 0");
         }
 
@@ -37,3 +42,43 @@ function updateTimerDisplay() {
 updateTimerDisplay(); 
 
 
+////////////////////////////////////////////////////////////////////////////
+
+let setRandomHit = 5;
+const hitElement = document.querySelector("#hit");
+
+function updateRandomHit() {
+
+    setRandomHit = Math.floor((Math.random() * 10) + 1);
+    hitElement.textContent = setRandomHit;
+}
+
+updateRandomHit();
+
+
+////////////////////////////////////////////////////////////////////////////
+
+const scoreElement = document.querySelector("#score");
+let score = 0;
+
+function updateScore(){
+    score += 10;
+    scoreElement.textContent = score;
+}
+
+// updateScore();
+
+////////////////////////////////////////////////////////////////////////////
+
+
+const screenBottom = document.querySelector("#screen");
+
+screenBottom.addEventListener("click" , function(event) {
+    let clickedNumber = Number(event.target.innerText);
+    
+    if (clickedNumber === setRandomHit){
+        updateScore();
+        updateRandomHit();
+        makeBubble();
+    }
+})
